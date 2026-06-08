a723ddb6250467bcb66fdbe87c562c0a15d01552
yvmou_ <yvmou@outlook.com>
2026-06-08 22:58:25 +0800

    2026.6.8-9

---
+++ blog-backend/blog-server/src/main/java/com/yvmoux/blog/aspect/RoleCheckAspect.java
@@ -0,0 +1,31 @@
+package com.yvmoux.blog.aspect;
+
+import com.yvmoux.blog.mapper.UserMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+@AllArgsConstructor
+public class RoleCheckAspect {
+
+    private final UserMapper userMapper;
+
+    /**
+     * 角色权限检查切入点
+     */
+    @Pointcut("execution(* com.yvmoux.blog.service.*.*(..)) && @annotation(com.yvmoux.blog.anno.RoleCheck)")
+    public void roleCheckPointcut() {}
+
+//    @Before("roleCheckPointcut()")
+//    public void roleCheck() {
+//        log.info("角色权限检查");
+//        userMapper.selectById()
+//
+//    }
+}
